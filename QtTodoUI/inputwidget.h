ee2cf9c0a530015178aeb62eeda34bf89d687a55
seongho9 <seongho9276@gmail.com>
2024-08-14 18:53:11 +0900

    [FIX] input widget

--- QtTodoUI/inputwidget.h
+++ QtTodoUI/inputwidget.h
@@ -16,14 +16,21 @@ public:
     ~InputWidget();
     void setInput(QString input);
     void setDue(QString due);
+    void setId(unsigned int id);
+    void setUserName(QString userName);
 private:
     Ui::InputWidget *ui;
     QString input;
     QString due;
+    QString userName;
+
+    unsigned int id;
+    bool isEdit;
 
 signals:
 
-    void enterPressed(QString input, QString dueDate);
+    void enterPressed(QString userName, QString input, QString dueDate);
+    void editPressed(unsigned int id, QString userName, QString input, QString dueDate);
 };
 
 #endif // INPUTWIDGET_H
