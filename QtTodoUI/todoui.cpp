3243d453865e05d37ec087ecb7fc3ee841f37188
seongho9 <seongho9276@gmail.com>
2024-08-14 18:52:31 +0900

    [FIX] todo ui

--- QtTodoUI/todoui.cpp
+++ QtTodoUI/todoui.cpp
@@ -1,76 +1,182 @@
 #include "todoui.h"
 #include "./ui_todoui.h"
-#include <QListWidget>
+#include "finishenabletab.h"
 #include <QLabel>
+#include <vector>
+#include "Todo.h"
 
-TodoUI::TodoUI(QWidget *parent)
+TodoUI::TodoUI(DataManage* manager, QString userName, QWidget *parent)
     : QWidget(parent)
     , ui(new Ui::TodoUI)
 {
+    enabled = false;
+    enablingWidget = nullptr;
+    this->manager = manager;
+    this->userName = userName;
     ui->setupUi(this);
-    auto item1 = std::make_shared<TodoItem>(this);
-    item1->setDueDate("2022-04-05");
-    item1->setContent("Sample Task 1");
-    item1->setIsFinish(true);
-    item1->setIsDone(false);
 
-    auto item2 = std::make_shared<TodoItem>(this);
-    item2->setDueDate("2022-05-10");
-    item2->setContent("Sample Task 2");
-    item2->setIsFinish(false);
-    item2->setIsDone(true);
-
-    auto item3 = std::make_shared<TodoItem>(this);
-    item3->setDueDate("2022-05-15");
-    item3->setContent("Sample Task 3");
-    item3->setIsFinish(true);
-    item3->setIsDone(true);
-
-    auto item4 = std::make_shared<TodoItem>(this);
-    item4->setDueDate("2022-12-15");
-    item4->setContent("Sample Task 4");
-    item4->setIsFinish(true);
-    item4->setIsDone(true);
-    auto item5 = std::make_shared<TodoItem>(this);
-    item5->setDueDate("2021-12-15");
-    item5->setContent("Sample Task 5");
-    item5->setIsFinish(true);
-    item5->setIsDone(true);
-    v.push_back(item1);
-    v.push_back(item2);
-    v.push_back(item3);
-    v.push_back(item4);
-    v.push_back(item5);
-
-    showTodoList(v);
+    showTodoList();
 }
 
-
-TodoUI::~TodoUI()
+void TodoUI::loadTodo()
 {
-    delete ui;
-}
+    std::vector<Todo> data;
 
-void TodoUI::showTodoList(const QVector<std::shared_ptr<TodoItem>> &todoList)
-{
-    ui->Contents->clear();
-    for(const auto &itemPtr : todoList)
-    {
-        QListWidgetItem *listWidgetItem = new QListWidgetItem();
-        listWidgetItem->setSizeHint(itemPtr->sizeHint()); // Set size hint based on widget's size
-        ui->Contents->addItem(listWidgetItem); // Add item to QListWidget
-        ui->Contents->setItemWidget(listWidgetItem, itemPtr.get()); // Set the custom widget
+    bool isDone = manager->getFinishedData(data, userName.toStdString());
+
+    for(Todo todo : data){
+        TodoItem* item = new TodoItem();
+        qDebug()<<"load"<<todo.getIsDone();
+        item->setContent(QString::fromStdString(todo.getContent()));
+        item->setDueDate(QString::fromStdString(todo.getDueDate()));
+        item->setIsDone(todo.getIsDone());
+        item->setIsFinish(todo.getIsFin());
+        item->setId(todo.getId());
+
+
+        connect(item, &TodoItem::edit, this,
+               [&](unsigned int id, QString content, QString date){
+                    emit edit(id, userName, content, date);
+                });
+
+        connect(item, &TodoItem::remove,
+                [&](unsigned int id){
+                    manager->removeTodoData(userName.toStdString(), id);
+                    removeListItems();
+                    showTodoList();
+                });
+        if(todo.getIsDone()){
+            this->doneTodo.push_back(item);
+        }
+        else{
+            this->dueTodo.push_back(item);
+        }
+    }
+    bool isFin = manager->getCurrentData(data, userName.toStdString());
+
+    for(Todo todo : data){
+        TodoItem* item = new TodoItem();
+
+        item->setContent(QString::fromStdString(todo.getContent()));
+        item->setDueDate(QString::fromStdString(todo.getDueDate()));
+        item->setIsDone(todo.getIsDone());
+        item->setIsFinish(todo.getIsFin());
+        item->setId(todo.getId());
+
+        connect(item, &TodoItem::edit, this,
+                [&](unsigned int id, QString content, QString date)
+                {  emit edit(id, userName, content, date);   });
+
+        connect(item, &TodoItem::remove, this,
+                [&](unsigned int id){
+                    manager->removeTodoData(userName.toStdString(), id);
+                    removeListItems();
+                    showTodoList();
+                });
+        connect(item, &TodoItem::checked, this,
+                [&](unsigned int id, bool flag){
+                    qDebug() << "checked" << id << flag;
+                    if(flag){
+                        manager->checkTodo(id, this->userName.toStdString());
+                    }
+                    //showTodoList();
+                });
+        if(todo.getIsDone()){
+            this->doneTodo.push_back(item);
+        }
+        else{
+            this->dueTodo.push_back(item);
+        }
     }
 }
 
-// void TodoUI::EndshowTodoList(const QVector<std::shared_ptr<TodoItem>> &todoList)
-// {
-//     ui->Contents->clear();
-//     for(const auto &itemptr : todoList)
-//     {
-//         TodoItem *widget = new TodoItem(*itemptr);
-//         if(widget->
-//     }
+TodoUI::~TodoUI()
+{
+    removeListItems();
+    delete ui;
+}
+
+void TodoUI::showTodoList()
+{
+    ui->contents->clear();
+    loadTodo();
+
+    if(enabled){
+        for(auto todo: doneTodo){
+            qDebug() <<"show";
+            QListWidgetItem* item = new QListWidgetItem();
+            finishLayout.push_back(item);
+            item->setSizeHint(todo->sizeHint());
+            ui->contents->addItem(item);
+            ui->contents->setItemWidget(item, todo);
+        }
+    }
+
+    enablingWidget = new FinishEnableTab();
+    enableLayout = new QListWidgetItem();
+    enablingWidget->setEnable(this->enabled);
+    connect(enablingWidget, &FinishEnableTab::clickTab, this,
+            [&](bool enable){
+                this->enabled = !(this->enabled);
+
+                removeListItems();
+                showTodoList();
+            });
+
+    enableLayout->setSizeHint(enablingWidget->sizeHint());
+    ui->contents->addItem(enableLayout);
+    ui->contents->setItemWidget(enableLayout, enablingWidget);
+
+    for(auto todo: dueTodo){
+
+        QListWidgetItem* item = new QListWidgetItem();
+        item->setSizeHint(todo->sizeHint());
+        duringLayout.push_back(item);
+        ui->contents->addItem(item);
+        ui->contents->setItemWidget(item, todo);
+    }
+}
+
+void TodoUI::removeListItems()
+{
+    for(auto item: finishLayout){
+
+        ui->contents->removeItemWidget(item);
+        delete item;
+    }
+    ui->contents->removeItemWidget(enableLayout);
+    delete enableLayout;
+    delete enablingWidget;
+
+    for(auto item: duringLayout){
+        ui->contents->removeItemWidget(item);
+        delete item;
+    }
+    finishLayout.clear();
+    duringLayout.clear();
 
 
-// }
+    for(auto todo:doneTodo){
+        delete todo;
+    }
+    for(auto todo:dueTodo){
+        delete todo;
+    }
+    doneTodo.clear();
+    dueTodo.clear();
+
+    ui->contents->clear();
+}
+
+void TodoUI::setUser(QString name)
+{
+    this->userName = name;
+    if(name == ""){
+        removeListItems();
+    }
+}
+
+QString TodoUI::getUserName() const
+{
+    return this->userName;
+}
